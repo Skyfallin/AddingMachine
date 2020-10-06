ea3b4e612c08495a69c95dca1730e70c8e09930c
Skyfallin <dimetrius.hightower@gmail.com>
2020-10-06 14:55:58 -0700

    Second commit w/ functionality

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -7,12 +7,14 @@ package cse360assignment02;
 public class AddingMachine {
 
     private int total;
+    private String history;
 
     /**
      * Creates an adding machine and sets total to 0
      */
     public AddingMachine() {
         total = 0;  // not needed - included for clarity
+        history = "0";
     }
 
     /**
@@ -20,7 +22,7 @@ public class AddingMachine {
      * @return the current total of the adding machine
      */
     public int getTotal() {
-        return 0;
+        return total;
     }
 
     /**
@@ -28,6 +30,8 @@ public class AddingMachine {
      * @param value the value to be added to the total
      */
     public void add(int value) {
+        total += value;
+        history += " + " + value;
     }
 
     /**
@@ -35,19 +39,23 @@ public class AddingMachine {
      * @param value the value to be subtracted from the total
      */
     public void subtract(int value) {
+        total -= value;
+        history += " - " + value;
     }
 
     /**
      * Displays the history of each transaction of the machine
-     * @return a string representing the history of the transactions of the machine
+     * @return history, a string representing the history of the transactions of the machine
      */
     public String toString() {
-        return "";
+        return history;
     }
 
     /**
      * Clears the machine
      */
     public void clear() {
+        total = 0;
+        history = "0";
     }
 }
