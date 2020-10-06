91a9b2e3a42cf95f701b9a9c4d2a050421e43498
Skyfallin <dimetrius.hightower@gmail.com>
2020-10-06 14:50:21 -0700

    Initial commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,53 @@
+package cse360assignment02;
+
+/**
+ * The AddingMachine class contains simple logic for adding, subtracting, and displaying a contained value.
+ * @author Skyfallin
+ */
+public class AddingMachine {
+
+    private int total;
+
+    /**
+     * Creates an adding machine and sets total to 0
+     */
+    public AddingMachine() {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * Displays the value of the adding machine
+     * @return the current total of the adding machine
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * Adds a value to the total
+     * @param value the value to be added to the total
+     */
+    public void add(int value) {
+    }
+
+    /**
+     * Subtracts a value from the total
+     * @param value the value to be subtracted from the total
+     */
+    public void subtract(int value) {
+    }
+
+    /**
+     * Displays the history of each transaction of the machine
+     * @return a string representing the history of the transactions of the machine
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * Clears the machine
+     */
+    public void clear() {
+    }
+}
